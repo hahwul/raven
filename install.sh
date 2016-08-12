abaed3abb053177f96b856512e23f3ca9912748c
hahwul <hahwul@gmail.com>
2016-08-12 18:27:35 +0900

    v1.0

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo 'Install GEM'
 gem install colorize
 echo ' -> set command'
 MYPWD=`pwd`
-echo '#/usr/bin/ruby\nruby '$MYPWD'raven.rb' >> /usr/bin/raven
+echo '#/usr/bin/ruby
+ruby '$MYPWD'/raven.rb $*' >> /usr/bin/raven
 echo 'Finish. run a raven'
